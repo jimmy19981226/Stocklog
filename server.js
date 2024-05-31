779bb7f183c3e8b64af24580cd2706f4011283ec
Chih-Chun-Chen <chen1226@go.byuh.edu>
2024-05-30 22:03:00 -1000

    Add the testAPI endpoint and modify docker and server

--- server.js
+++ server.js
@@ -3,21 +3,34 @@ const express = require("express");
 const mongoose = require("mongoose");
 const entityRoutes = require("./routes/entityRoutes");
 
-const app = express();
 const port = process.env.PORT || 3000;
+const mongoUri =
+  process.env.MONGO_URI || "mongodb://localhost:27017/mydatabase";
 
+// Create Express app
+const app = express();
+
+// Middleware to parse JSON
 app.use(express.json());
 
+// Define the testAPI endpoint
+app.get("/testAPI", (req, res) => {
+  res.send("testAPI is working!");
+});
+
+// Use entity routes
+app.use("/api/entities", entityRoutes);
+
+// Connect to MongoDB and start the server
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(mongoUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Connected to MongoDB"))
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+    console.log("Connected to MongoDB");
+  })
   .catch((err) => console.error("Could not connect to MongoDB", err));
-
-app.use("/api/entities", entityRoutes);
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
