1462bbd7e5dc9dd4814757883eb8460438032d62
Chih-Chun-Chen <chen1226@go.byuh.edu>
2024-06-08 16:52:23 -1000

    Change the auth to fix the upadate user

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,28 +1,26 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
 
-const auth = async (req, res, next) => {
-  const token = req.header("Authorization")?.replace("Bearer ", "");
-  if (!token) {
+module.exports = (req, res, next) => {
+  const authHeader = req.header("Authorization");
+  if (!authHeader)
     return res
       .status(401)
       .json({ message: "Access denied. No token provided." });
-  }
+
+  const token = authHeader.split(" ")[1]; // Bearer <token>
+  if (!token)
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
 
   try {
     const decoded = jwt.verify(
       token,
       process.env.JWT_SECRET || "your_jwt_secret"
     );
-    const user = await User.findById(decoded.userId);
-    if (!user) {
-      return res.status(401).json({ message: "Invalid token." });
-    }
-    req.user = user; // Attach the user object to the request
-    next(); // Call the next middleware or route handler
-  } catch (err) {
+    req.user = decoded;
+    next();
+  } catch (ex) {
     res.status(400).json({ message: "Invalid token." });
   }
 };
-
-module.exports = auth;
